8272452d8edfa8954c9649a45667491e870389d6
Samson Kwaku Nkrumah <samsonnkrumah253@gmail.com>
2021-11-17 22:21:28 +0530

    memory list card added

---
+++ my.js
@@ -0,0 +1,4 @@
+var script = document.createElement('script');
+script.src = "./jquery-3.6.0.min.js";
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
